f95230ce978ed4fd76c3cbe0d1972b153f49839e
williamv.anderson@gmail.com <williamv.anderson@gmail.com>
2011-05-16 19:47:00 -0400

    First Commit - We have color coded product listings, Houston

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5dde9281d1e3ffbf12472108bb1c88c3fd03b75c580c4584efd7dd72f0cc7cc8bc804158a48d1b74a23a753d665ad60f7818047f7a4a254a6c89710772c0b0ae'
